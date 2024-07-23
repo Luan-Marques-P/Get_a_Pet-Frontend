3fa976c3f7a6e99c2bac8749b16b321305425561
Luan-Marques-P <luanmarques10@outlook.com.br>
2024-07-23 17:05:29 -0300

    Criação de Perfil

--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -1,18 +1,31 @@
+import { useState, useContext } from 'react'
+
 import Input from '../../form/Input'
 import { Link } from 'react-router-dom'
 
 import styles from '../../form/Form.module.css'
 
+/* contexts */
+import { Context } from '../../../context/UserContext'
+
 function Register() {
+    const [user, setUser] = useState({})
+    const { register } = useContext(Context)
+
 
     function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
 
+    function handleSubmit(e) {
+        e.preventDefault()
+        register(user)
     }
 
     return (
         <section className={styles.form_container}>
             <h1>Registrar</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <Input text='Nome' type='text' name='name' placeholder='Digite o seu nome' handleOnChange={handleChange} />
                 <Input text='Telefone' type='text' name='phone' placeholder='Digite o seu telefone' handleOnChange={handleChange} />
                 <Input text='E-mail' type='email' name='email' placeholder='Digite o e-mail' handleOnChange={handleChange} />
