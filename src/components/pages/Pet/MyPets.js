839bcc9d4779f18cb040148f0e945c73948c4d5e
Luan-Marques-P <luanmarques10@outlook.com.br>
2024-07-26 17:10:30 -0300

    Conclusão do front

--- src/components/pages/Pet/MyPets.js
+++ src/components/pages/Pet/MyPets.js
@@ -1,22 +1,126 @@
+import api from '../../../utils/api'
+
+import styles from './Dashboard.module.css'
+
 import { useState, useEffect } from 'react'
 
 import { Link } from 'react-router-dom'
 
-function MyPets() {
-    const [pets, setPets] = useState([])
+import RoundedImage from '../../layouts/RoundedImage'
 
-    return (
-        <section>
-            <div>
-                <h1>MyPets</h1>
-                <Link to='/pet/add'>Cadastrar Pets</Link>
+/* hooks */
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+function MyPets() {
+  const [pets, setPets] = useState([])
+  const [token] = useState(localStorage.getItem('token') || '')
+  const { setFlashMessage } = useFlashMessage()
+
+  useEffect(() => {
+    api
+      .get('/pets/mypets', {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setPets(response.data.pets)
+      })
+  }, [token])
+
+  async function removePet(id) {
+    let msgType = 'success'
+
+    const data = await api
+      .delete(`/pets/${id}`, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        const updatedPets = pets.filter((pet) => pet._id != id)
+        setPets(updatedPets)
+        return response.data
+      })
+      .catch((err) => {
+        console.log(err)
+        msgType = 'error'
+        return err.response.data
+      })
+
+    setFlashMessage(data.message, msgType)
+  }
+
+  async function concludeAdoption(id) {
+    let msgType = 'success'
+
+    const data = await api
+      .patch(`/pets/conclude/${id}`, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        return response.data
+      })
+      .catch((err) => {
+        console.log(err)
+        msgType = 'error'
+        return err.response.data
+      })
+
+    setFlashMessage(data.message, msgType)
+  }
+
+  return (
+    <section>
+      <div className={styles.petslist_header}>
+        <h1>Meus Pets Cadastrados</h1>
+        <Link to="/pet/add">Cadastrar Pet</Link>
+      </div>
+      <div className={styles.petslist_container}>
+        {pets.length > 0 &&
+          pets.map((pet) => (
+            <div key={pet._id} className={styles.petlist_row}>
+              <RoundedImage
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <>
+                    {pet.adopter && (
+                      <button
+                        className={styles.conclude_btn}
+                        onClick={() => {
+                          concludeAdoption(pet._id)
+                        }}
+                      >
+                        Concluir adoção
+                      </button>
+                    )}
+
+                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                    <button
+                      onClick={() => {
+                        removePet(pet._id)
+                      }}
+                    >
+                      Excluir
+                    </button>
+                  </>
+                ) : (
+                  <p>Pet já adotado</p>
+                )}
+              </div>
             </div>
-            <div>
-                { pets.length > 0 && <p>Meus Pets cadastrados.</p> }
-                { pets.length === 0 && <p>Não há Pets cadastrados</p> }
-            </div>
-        </section>
-    )
+          ))}
+        {pets.length === 0 && <p>Ainda não há pets cadastrados!</p>}
+      </div>
+    </section>
+  )
 }
 
 export default MyPets
